5fce175cbd8430d62dd2dd5d5d7b5b781bb22eb3
Pavel Liashkevich <paul8paul8@mail.ru>
2024-04-23 19:27:30 +0300

    feat: add homework-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+	state: UserType[],
+	action: ActionType
+): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case 'sort': {
+			// by name
+			const copyState = [...state]
 
-            return state // need to fix
-        }
-        case 'check': {
+			return copyState.sort((a: UserType, b: UserType) =>
+				a.name > b.name
+					? action.payload === 'up'
+						? 1
+						: -1
+					: action.payload === 'up'
+					? -1
+					: 1
+			)
+		}
+		case 'check': {
+			const copyState = [...state]
 
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+			return copyState.filter((a: UserType) => a.age > 18)
+		}
+		default:
+			return state
+	}
 }
