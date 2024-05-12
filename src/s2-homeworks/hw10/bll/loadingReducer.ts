18c4a74f141189fdc800bc551e13bbd97fe78f25
Pavel Liashkevich <paul8paul8@mail.ru>
2024-05-12 17:02:26 +0300

    feat: add homework-10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,31 @@
 const initState = {
-    isLoading: false,
+	isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+export type initStateType = typeof initState
 
-        default:
-            return state
-    }
+export const loadingReducer = (
+	state: initStateType = initState,
+	action: LoadingActionType
+): initStateType => {
+	switch (action.type) {
+		case 'CHANGE_LOADING': {
+			return {
+				...state,
+				isLoading: action.isLoading,
+			}
+		}
+		default:
+			return state
+	}
 }
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
+	type: 'CHANGE_LOADING'
+	isLoading: boolean
 }
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+	type: 'CHANGE_LOADING',
+	isLoading,
 })
